561ccfd3e8f029b511dfeb46d54028e5674fb19a
Paquetaaa <121290992+Paquetaaa@users.noreply.github.com>
2024-06-06 14:02:08 +0200

    Add files via upload

--- Etape4/reponse_fichier.php
+++ Etape4/reponse_fichier.php
@@ -3,55 +3,44 @@
 
 <head>
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
 <body>
+    <!-- PARTIE HTML -->
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="fichier">
+        <input type="submit" value="Envoyer">
     <!-- PARTIE PHP -->
     <?php
     if(isset($_POST) && !empty($_POST)){
-        ?> <pre> <?php print_r($_POST); ?> </pre> <?php
-        ?> <pre> <?php print_r($_FILES); ?> </pre> <?php
+        echo '<pre>'; print_r($_POST); echo '</pre>';
+        echo '<pre>'; print_r($_FILES); echo '</pre>';
 
         if($_FILES['fichier']['error'] == 0){
-            //Upload le fichier dans le dossier uploads
-            move_uploaded_file($_FILES['fichier']['tmp_name'], 'uploads/' . $_FILES['fichier']['name']);
-            echo 'Fichier envoyé avec succès';
+            // 1.5 Mo maximum pour le poids du fichier
             if($_FILES['fichier']['size']> 1500000){
                 $error = 'Le fichier est trop lourd';
             }
+
+            // Vérifier le type de fichier
+            $extensions = strrchr($_FILES['fichier']['name'], '.');
+            if($extensions != '.jpg'){
+                $error = 'Le fichier n\'est pas une image jpg';
+            }
+
+            if(!isset($error)){
+                //Upload le fichier dans le dossier uploads
+                move_uploaded_file($_FILES['fichier']['tmp_name'], 'uploads/' . $_FILES['fichier']['name']);
+                echo 'Fichier envoyé avec succès';
+            }else{
+                echo $error;
+            }
         }else{
-            $error = 'Erreur lors de l\'envoi du fichier';
+            echo 'Erreur lors de l\'envoi du fichier';
         }
-        // 1.5 Mo maximum pour le poids du fichier
-        
-
     }
-
-
-
-    
-
-
-
     ?>
-
-    <!-- FORMULAIRE HTML -->
-
-    <div style="color:red">
-        <p>
-            <?php if(isset($error)) echo $error; ?>
-        </p>
-    </div>
-    <form method="POST" action="#" enctype="multipart/form-data">
-
-        <input type="file" name="fichier" value="">
-        <input type="submit" value="Envoyer" value="Charger le fichier">
-    </form>
-
-
 </body>
-
 </html>
