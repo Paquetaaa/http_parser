ddfef294773affa1c9c6c047379b8f5c1bb07fab
Paquetaaa <121290992+Paquetaaa@users.noreply.github.com>
2024-06-07 09:42:37 +0200

    Update FCGI.h

--- Etape4/FCGI.h
+++ Etape4/FCGI.h
@@ -27,21 +27,22 @@ typedef struct blabla {
 static int createSocket(char *ip, int port);
 
 // From others
-void Create_and_Send_AbortRequest(int fd, unsigned short requestId);
-void Create_and_Send_BeginRequest(int fd, unsigned short requestId);
-void Create_and_Send_GetValuesRequest(int fd);
-void sendStdin(int fd, int id, char* data);
-void sendWebData(int fd, unsigned char type, unsigned short requestId, char *data, unsigned int len);
-size_t readSocket(int fd, char *buf, size_t len);
+
+
 void readData(int fd, FCGI_Header *h, size_t *len);
 int addNameValuePair(FCGI_Header *header, char *name, char *value);
-void writeSocket(int fd, FCGI_Header *h, unsigned int len);
 void writeLen(int len, char **p);
 char* lecture_reponse(int socket);
-void sendRequete();
+
 
 // Ours
 nameValuePair* ecrire_http_header(int* nb_http_headers);
 nameValuePair* ecrire_fcgi_header(int* nb_fcgi_headers);
 void createRequeteParams(int fd);
 void createEmptyParams(int fd);
+void Create_and_Send_AbortRequest(int fd, unsigned short requestId);
+void Create_and_Send_BeginRequest(int fd, unsigned short requestId);
+void Create_and_Send_GetValuesRequest(int fd);
+void sendRequete();
+void sendStdin(int fd, int id, char* data);
+void sendWebData(int fd, unsigned char type, unsigned short requestId, char *data, unsigned int len);
